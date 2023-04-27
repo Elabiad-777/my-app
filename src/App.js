5fc287c6e22b358a1321c7c2be7d3d27e7fc61f2
Elabiad-777 <ah1148504799@gmail.com>
2023-04-28 01:20:20 +0300

    #elabiad

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>elabiad</h1>
+      <h1>elabiad</h1>
+      <h1>elabiad</h1>
+      <h1>elabiad</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+
